e34764e39f18b510807b14b985c916d40a4cb7ee
Mutuba <danielmutubait@gmail.com>
2022-09-16 12:18:23 +0300

    finish user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :urls, only: %i[index new create]
-  # get 'urls/all', to: 'urls#index'
-  # get 'urls/new', to: 'urls#new'
-  # get 'urls/create', to: 'urls#create'
   get 'urls/show', to: 'urls#show'
 
   root 'urls#index'

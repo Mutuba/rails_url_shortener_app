7cb8aa20ec7480a232c02ef7d38ba8c9a9b8769b
Mutuba <danielmutubait@gmail.com>
2022-09-22 21:01:50 +0300

    refactor urls listing

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,6 +3,10 @@ class UrlsController < ApplicationController
   before_action :set_url, only: %i[show]
   before_action :authenticate_user!
 
+  def index
+    @urls = current_user.urls.page(params[:page])
+  end
+
   def show
     render 'errors/404', status: 404 if @url.nil?
     @url.update_attribute(:click, @url.click + 1)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   mount ActionCable.server => '/cable'
-  resources :urls, only: %i[new]
+  resources :urls, only: %i[new index]
 
   post 'urls/create', to: 'urls#create'
   get 'urls/show', to: 'urls#show'

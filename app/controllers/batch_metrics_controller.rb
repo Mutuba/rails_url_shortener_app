d0ec7b1494bda7311520aa5b024fe48cb7b5acf3
Mutuba <danielmutubait@gmail.com>
2022-09-23 08:04:03 +0300

    do conditionals for when we don't have any urls and batches

--- app/controllers/batch_metrics_controller.rb
+++ app/controllers/batch_metrics_controller.rb
@@ -1,16 +1,9 @@
 class BatchMetricsController < ApplicationController
-  require 'securerandom'
   before_action :set_url, only: %i[download_status batch_urls]
   before_action :authenticate_user!
 
-  def index
-    @batches = current_user.batches.includes(:urls)
-  end
-
   def batch_urls
     redirect_to root_path, notice: 'Oops, you no authorized to view the resource' if @batch.user != current_user
-
-    # binding.pry
 
     @urls = @batch.urls.page(params[:page])
     render template: 'batch_metrics/batch_urls', locals: { urls: @urls }
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   get 'urls/show', to: 'urls#show'
   get 'batch/stats', to: 'batch_metrics#batch_stats'
   get 'batch/download_status', to: 'batch_metrics#download_status'
-  get 'batches', to: 'batch_metrics#index'
   get 'batch_urls', to: 'batch_metrics#batch_urls'
 
-  root 'home#index'
+  root 'urls#index'
 end

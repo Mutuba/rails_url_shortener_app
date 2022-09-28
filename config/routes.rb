daf07c6a9b90f5232e2f93f8d330fe2c8598ebee
Mutuba <danielmutubait@gmail.com>
2022-09-28 09:28:52 +0300

    fix next and previous on batch urls page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'urls/show', to: 'urls#show'
   get 'batch/stats', to: 'batch_metrics#batch_stats'
   get 'batch/upload_status', to: 'batch_metrics#upload_status'
-  # post 'batch/upload_status', to: 'batch_metrics#upload_status'
+  get 'batch_urls', to: 'batch_metrics#batch_urls'
   post 'batch_urls', to: 'batch_metrics#batch_urls'
 
   root 'urls#index'

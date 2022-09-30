fea9b2ec44944ae350567e56228fb9ac7c73975b
Mutuba <danielmutubait@gmail.com>
2022-09-30 23:04:51 +0300

    add case for invalid password with existing user

--- spec/sign_in_spec.rb
+++ spec/sign_in_spec.rb
@@ -19,23 +19,23 @@ describe 'User signs in', type: :system do
     expect(page).to have_current_path get_started_path
   end
 
-  #   scenario 'invalid with unregistered account' do
-  #     fill_in 'user_email', with: Faker::Internet.email
-  #     fill_in 'user_password', with: 'FakePassword123'
-  #     click_button 'sign in'
+    scenario 'invalid with unregistered account' do
+      fill_in 'user_email', with: Faker::Internet.email
+      fill_in 'user_password', with: 'FakePassword123'
+      click_button 'Log in'
 
-  #     expect(page).to have_no_text 'Welcome back'
-  #     expect(page).to have_text 'Invalid Email or password.'
-  #     expect(page).to have_no_link 'Sign Out'
-  #   end
+      expect(page).to have_no_text 'Signed in successfully.'
+      expect(page).to have_text 'Invalid Email or password.'
+      expect(page).to have_no_link 'Sign Out'
+    end
 
-  #   scenario 'invalid with invalid password' do
-  #     fill_in 'user_email', with: @user.email
-  #     fill_in 'user_password', with: 'FakePassword123'
-  #     click_button 'sign in'
+    scenario 'invalid with invalid password' do
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: 'FakePassword123'
+      click_button 'Log in'
 
-  #     expect(page).to have_no_text 'Welcome back'
-  #     expect(page).to have_text 'Invalid Email or password.'
-  #     expect(page).to have_no_link 'Sign Out'
-  #   end
+      expect(page).to have_no_text 'Signed in successfully.'
+      expect(page).to have_text 'Invalid Email or password.'
+      expect(page).to have_no_link 'Sign Out'
+    end
 end

e33ff1a2421605fadc10919b2605e90143fa3a2c
Mutuba <danielmutubait@gmail.com>
2022-09-30 09:41:39 +0300

    add long url length validation

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Url, type: :model do
   # Association test
   it { should belong_to(:user) }
   it { should belong_to(:batch) }
+  it { is_expected.not_to allow_value(Faker::Lorem.characters(number: 10)).for :long_url }
 
   # Validation test
   it { should validate_presence_of(:user_id) }

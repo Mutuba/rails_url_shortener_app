a89cdf8363f019cdbdaa51c844247bc446fa0894
Mutuba <danielmutubait@gmail.com>
2022-10-01 22:52:29 +0300

    complete

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,14 +2,6 @@
 
 # spec/factories/user.rb
 FactoryBot.define do
-  # sequence :email do |n|
-  #   "email_#{n}@tinyfyurls.com"
-  # end
-
-  # sequence :password do |n|
-  #   "1404555121frddsa#{n}"
-  # end
-
   factory :user do
     email { 'jane.doe@hey.com' }
     password { 'SecretPassword123' }

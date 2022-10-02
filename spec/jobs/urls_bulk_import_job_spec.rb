eb0d3734510473382212da41bb0474a864c16bfb
Mutuba <danielmutubait@gmail.com>
2022-10-02 19:38:10 +0300

    add jobs specific job

---
+++ spec/jobs/urls_bulk_import_job_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UrlsBulkImportJob, type: :job do
+  describe '#perform_later' do
+    let(:base_url) { Faker::Internet.url }
+    let(:string_file_path) { "/tmp/bulk-import #{SecureRandom.uuid}.csv" }
+
+    before do
+      allow(BulkUrlsImportService).to receive(:call)
+      @user = create :user
+    end
+
+    it 'uploads a urls' do
+      UrlsBulkImportJob.perform_later(string_file_path, base_url, @user)
+      expect(UrlsBulkImportJob).to have_been_enqueued.exactly(:once)
+    end
+  end
+end

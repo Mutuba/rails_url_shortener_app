a92f9611b5eb46da701be4ffd56f7bf951dc16e1
Mutuba <danielmutubait@gmail.com>
2022-10-04 11:25:46 +0300

    complete requests specs for urls controller

--- spec/factories/url.rb
+++ spec/factories/url.rb
@@ -3,7 +3,9 @@
 # spec/factories/url.rb
 FactoryBot.define do
   factory :url do
-    long_url { Faker::Internet.url }
+    long_url { 'http://example.com/foobar.html' }
     short_url { Faker::Internet.url }
+    batch
+    user
   end
 end

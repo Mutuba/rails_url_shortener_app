8295a4de4b84c9fbbb92186baf035b2c32319967
Mutuba <danielmutubait@gmail.com>
2022-10-05 12:11:54 +0300

    refactor urls bulk import system tests

--- spec/system/urls_bulk_upload_spec.rb
+++ spec/system/urls_bulk_upload_spec.rb
@@ -24,9 +24,15 @@ describe 'User uploads urls using csv file', type: :system do
     click_link 'Upload Urls'
     page.attach_file('url_file', Rails.root.join('spec/fixtures/test_file.csv'))
     click_button 'Upload File'
+    expect(UrlsBulkImportJob).to have_been_enqueued.exactly(:once)
     perform_enqueued_jobs
     assert_performed_jobs 1
     expect(page).to have_content('Upload in progress. Please sit tight')
+    click_link 'Batch Metrics'
+    expect(page).to have_content('Batch Metrics')    
+    click_on(class: 'accordion-button')
+    expect(page).to have_button('View Batch Urls')
+    click_on 'View Batch Urls'
   end
 
   scenario 'File missing on upload' do

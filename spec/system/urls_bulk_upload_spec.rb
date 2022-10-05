ef5b9fc919f0c190bf1cdc069704f7461cbcfe6a
Mutuba <danielmutubait@gmail.com>
2022-10-05 10:17:01 +0300

    refactor urls bulk import system tests

--- spec/system/urls_bulk_upload_spec.rb
+++ spec/system/urls_bulk_upload_spec.rb
@@ -3,17 +3,24 @@
 require 'rails_helper'
 require 'csv'
 
+RSpec.shared_context :login_user do
+  let(:user) { create(:user) }
+  before do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Log in'
+  end
+end
+
 describe 'User uploads urls using csv file', type: :system do
+  include_context :login_user
+
   before do
     allow(SecureRandom).to receive(:uuid).and_return('12abcd1234')
-    @user = create :user
-    visit new_user_session_path
   end
 
   scenario 'POST #create' do
-    fill_in 'user_email', with: @user.email
-    fill_in 'user_password', with: @user.password
-    click_button 'Log in'
     click_link 'Upload Urls'
     page.attach_file('url_file', Rails.root.join('spec/fixtures/test_file.csv'))
     click_button 'Upload File'
@@ -23,9 +30,6 @@ describe 'User uploads urls using csv file', type: :system do
   end
 
   scenario 'File missing on upload' do
-    fill_in 'user_email', with: @user.email
-    fill_in 'user_password', with: @user.password
-    click_button 'Log in'
     click_link 'Upload Urls'
     click_button 'Upload File'
     expect(page).to have_content('Oops! File missing')

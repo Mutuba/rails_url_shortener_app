80aa5a8c75a59639d93e2fdda124160d7636684a
Mutuba <danielmutubait@gmail.com>
2023-05-11 14:58:30 +0300

    clean sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,29 +2,35 @@
 
 require 'sidekiq/web'
 require 'sidekiq-status'
-
-Sidekiq.configure_server do |config|
-  # config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-
-
-end
+# require 'sidekiq-unique-jobs'
 
 Sidekiq.configure_client do |config|
-  # config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) }
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = {
+    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'),
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 
-end
-
-Sidekiq.configure_client do |config|
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes.to_i
+  config.client_middleware do |chain|
+    # chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
 
 Sidekiq.configure_server do |config|
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
+  config.redis = {
+    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'),
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes.to_i
+
+  config.client_middleware do |chain|
+    # chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  # config.server_middleware do |chain|
+  #   chain.add SidekiqUniqueJobs::Middleware::Server
+  # end
+
+  # SidekiqUniqueJobs::Server.configure(config)
 end

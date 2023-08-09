9ec4f40f6384b10fb2a1a08a1284198629619720
Mutuba <danielmutubait@gmail.com>
2023-08-09 21:50:07 +0300

    refactor the bulk upload urls service

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -29,7 +29,7 @@ class UrlsController < ApplicationController
     end
 
     base_url = request.base_url
-    file_path = Rails.root.join("/tmp/bulk-import #{SecureRandom.uuid}.csv")
+    file_path = Rails.root.join('public', "bulk-import-#{SecureRandom.uuid}.csv")
     File.write(file_path, params[:url][:file].read)
     UrlsBulkImportJob.perform_later file_path.to_path, base_url, current_user
 
--- app/services/bulk_urls_import_service.rb
+++ app/services/bulk_urls_import_service.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 # handles the bulk upload process
+
 class BulkUrlsImportService < ApplicationService
   require 'csv'
-  require 'securerandom'
-  require 'faker'
 
   def initialize(params)
     super()
@@ -20,67 +19,65 @@ class BulkUrlsImportService < ApplicationService
   private
 
   def generate_short_url
-    "#{@base_url}/#{rand(36**8).to_s(36)}"
+    "#{@base_url}/#{SecureRandom.hex(4)}"
   end
 
   def sanitize_url(long_url_arg)
-    long_url_arg.strip!
-    sanitize_url = long_url_arg.downcase.gsub(%r{(https?://)|(www\.)}, '')
-    "http://#{sanitize_url}"
+    long_url_arg.strip.downcase.sub(%r{(https?://)|(www\.)}, 'http://')
+  end
+
+  def create_batch
+    Batch.create(
+      name: "#{Faker::TvShows::GameOfThrones.house} #{SecureRandom.hex(5)}",
+      user: @current_user
+    )
   end
 
   def process_csv!
-    batch = Batch.create(
-      name: "#{Faker::TvShows::GameOfThrones.house} #{SecureRandom.hex(5)}",
-      user: @current_user,
-    )
+    batch = create_batch
     urls_array = []
+  
     begin
       CSV.foreach(@file_path, headers: true) do |row|
         url_hash = Url.new
-
+  
         url_hash.batch = batch
         url_hash.long_url = sanitize_url(row[0])
         url_hash.short_url = row[1].nil? ? generate_short_url : "#{@base_url}/#{row[1]}"
         url_hash.created_at = Time.zone.now
         url_hash.updated_at = Time.zone.now
         url_hash.user_id = @current_user.id
+  
         urls_array << url_hash
       end
-    rescue Errno::ENOENT => e
+    rescue Errno::ENOENT, Errno::EACCES, CSV::MalformedCSVError => e
       Rails.logger.info e.message
-    rescue Errno::EACCES => e
-      Rails.logger.info e.message
-    rescue CSV::MalformedCSVError => e
-      Rails.logger.info e.message
+    ensure
+      File.delete(@file_path) if File.exist?(@file_path)
     end
-
+  
+    instance = import_urls(urls_array, batch)
+    record_failed_urls(instance.failed_instances)
+    record_batch_metrics(instance, batch)
+  end
+    
+  def import_urls(urls_array, batch)
     my_proc = lambda { |_, num_batches, current_batch_number, _|
-      # send an email, post to a websocket,
-      # update slack, alert if import is taking too long, etc.
-      # the pipe takes _rows_size, num_batches, current_batch_number, _batch_duration_in_secs
-      # binding.pry
       progress = (current_batch_number * 100) / num_batches
       ActionCable.server.broadcast("#{@current_user.id}#{batch.id}", { content: progress })
     }
-   
-    instance = Url.import urls_array, batch_size: 2, batch_progress: my_proc,
-                                      returning: :long_url
 
-    # instance.failed_instances.size.positive? &&
-      
-    # record_failed_urls(instance.failed_instances)
-    # record_batch_metrics(instance, batch)
+    Url.import urls_array, batch_size: 2, batch_progress: my_proc
   end
 
   def record_failed_urls(failed_instances)
-    failed_instances.size.positive? &&
-      failed_instances.each_slice(2).each do |array_instance|
-        array_instance.each do |element|
-          FailedUrl.create(long_url: element.long_url, batch: element.batch,
-                           user_id: @current_user.id)
-        end
+    return unless failed_instances.size.positive?
+
+    failed_instances.each_slice(2) do |array_instance|
+      array_instance.each do |element|
+        FailedUrl.create(long_url: element.long_url, batch: element.batch, user_id: @current_user.id)
       end
+    end
   end
 
   def record_batch_metrics(instance, batch)

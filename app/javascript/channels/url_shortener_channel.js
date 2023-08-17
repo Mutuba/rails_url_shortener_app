4f067d4c3055d4742e6b0ac076f31f9a04016d1a
Mutuba <danielmutubait@gmail.com>
2023-08-17 11:52:10 +0300

    fix controller tests

--- app/javascript/channels/url_shortener_channel.js
+++ app/javascript/channels/url_shortener_channel.js
@@ -10,11 +10,22 @@ consumer.subscriptions.create(
       const progressWidth = document.getElementById("progress-width");
       const progressText = document.getElementById("progress-text");
       const progressText1 = document.getElementById("progress-text-percentage");
+      const progressAlert = document.getElementById("progress-alert");
 
       progressBar.style.visibility = "visible";
       progressWidth.style.width = `${data.content}%`;
       progressText.innerText = `${data.content}%`;
       progressText1.innerText = `${data.content}%`;
+
+      if (data.content === 100) {
+        progressText.innerText = "Upload complete";
+        progressText1.innerText = "100%";
+        progressAlert.innerText = "Upload complete";
+
+        subscription.unsubscribe();
+      } else {
+        progressText.innerText = `Uploading data to our servers: ${progressText.innerText}`;
+      }
     },
   }
 );

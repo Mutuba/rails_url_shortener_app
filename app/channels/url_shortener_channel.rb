0eaf2e82d95537fb47dc50f24c1fd3a6805191f6
Mutuba <danielmutubait@gmail.com>
2023-09-13 16:45:07 +0300

    fix typos

--- app/channels/url_shortener_channel.rb
+++ app/channels/url_shortener_channel.rb
@@ -4,7 +4,6 @@
 class UrlShortenerChannel < ApplicationCable::Channel
   def subscribed
     reject if params[:batch_id].blank?
-
     stream_from "#{current_user&.id}#{params[:batch_id]}"
     Rails.logger.info "streaming for user id #{current_user&.id} in UrlShortenerChannel"
   end
